487b67502467dc2dffaf274c8457224b225a7084
Sakthi Murugan <sakthimuruganinbox@gmail.com>
2023-03-27 01:42:57 +0530

    Added delete functionality in browsing mode only. New tasks are now added to sessionStorage too

--- main.js
+++ main.js
@@ -1,41 +1,88 @@
 //Selectors
 const addTaskField = document.querySelector('.addTaskField');
 const addTaskButton = document.querySelector('.addTaskButton');
-
 const taskList = document.querySelector('.taskList');
+
+
+//Init
+let taskCount = 0;
+addTaskField.textContent = '';
+addTaskField.focus()
 
 //Listeners
 addTaskButton.addEventListener('click', addTaskToList); 
 
+/*Listener for the dynamically created deleteButton */
+document.body.addEventListener( 'click', function (event) {
+    if( event.target.id == 'deleteButton' ) {
+      deleteTaskFromList(event.target);
+    };
+});
+
 //Functions
 function addTaskToList() {
+
     //Validation to check if anything is input
     let fieldValue = addTaskField.value;
     if (fieldValue.length === 0) {
-        alert('Please enter     a task');
+        alert('Please enter a task');
         return;
     }
 
+    //Update taskCount
+    if (taskCount===0) {
+        nothingToDoText = document.querySelector('.nothingToDoText')
+        nothingToDoText.textContent = '';
+    }
+    taskCount += 1;
+
     //Create the new task elements
-    let taskItem = document.createElement('div');
+    let taskItem  = createNewListItem(fieldValue);
+    //Add the new task to the page
+    taskList.appendChild(taskItem);
+
+    //Add fieldvalue into sessionStorage
+    sessionStorage.setItem(taskCount, fieldValue.toString())
+
+    //Clear the input and bring it to focus
+    addTaskField.value = '';
+    addTaskField.focus();
+
+}
+
+function createNewListItem (fieldValue) {
+    /*
+    Structire of a listItem:
+    <div> 
+    span></span>
+    <button><button/>
+    </div> */
+
+    let taskItem= document.createElement('div');
     taskItem.setAttribute('class', 'taskItem');
 
-    let taskName = document.createElement('span');
+    let taskName = document.createElement('span'); 
     taskName.setAttribute('class', 'taskName');
     taskName.textContent = fieldValue;
 
     let deleteButton = document.createElement('button');
     deleteButton.setAttribute('class', 'deleteButton');
+    deleteButton.setAttribute('id', 'deleteButton');
     deleteButton.textContent = 'Delete'
-
     
     taskItem.appendChild(taskName);
     taskItem.appendChild(deleteButton);
 
-    //Add the new task to the page
-    taskList.appendChild(taskItem);
+    return taskItem;
+}
 
-    //Clear the input and bring it to focus
-    addTaskField.value = '';
+function deleteTaskFromList (target) {
+    taskList.removeChild(target.parentNode);
     addTaskField.focus();
+    taskCount -= 1;
+
+    if (taskCount === 0) {
+        nothingToDoText = document.querySelector('.nothingToDoText')
+        nothingToDoText.textContent = 'Nothing to do :)';
+    }
 }
